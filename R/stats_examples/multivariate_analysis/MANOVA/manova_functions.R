0f1ff4a8eb770f487c619e3ea30305b419b7b0b2
Sean Ammirati <sean.ammirati75@myhunter.cuny.edu>
2018-08-05 20:55:53 -0400

    Adding seperated code, more examples

--- R/stats_examples/multivariate_analysis/MANOVA/manova_functions.R
+++ R/stats_examples/multivariate_analysis/MANOVA/manova_functions.R
@@ -1,7 +1,4 @@
-## Here we will be  doing MANOVA for the dataset below. We want to know if the means of tear, gloss and opacity are statistically 
-## significantly different from one another depending on the rate groups -- Low or High. 
-
-return_results <- function(dataset, group_column) {
+return_MANOVA_results <- function(dataset, group_column) {
   F_dists <- find_approx_F_dist(dataset, group_column)
   results <- 1 - sapply(X = F_dists[c('Wilks','Roy','Pillai','Hotelling')], FUN = function(obj) pf(obj[1], obj[2], obj[3]))
   results <- rbind(p_f = results, statistic = F_dists$untransformed[1:4],
@@ -104,18 +101,3 @@ FapproxforHotelling <- function(H, Vh, p, Ve) {
   return(c(Fapprox = Fapprox, df1 = a, df2 = b))
 }
 
-## Example: 
-
-## Data from manova example: Krzanowski (1998, p. 381)
-
-tear <- c(6.5, 6.2, 5.8, 6.5, 6.5, 6.9, 7.2, 6.9, 6.1, 6.3,
-          6.7, 6.6, 7.2, 7.1, 6.8, 7.1, 7.0, 7.2, 7.5, 7.6)
-gloss <- c(9.5, 9.9, 9.6, 9.6, 9.2, 9.1, 10.0, 9.9, 9.5, 9.4,
-           9.1, 9.3, 8.3, 8.4, 8.5, 9.2, 8.8, 9.7, 10.1, 9.2)
-opacity <- c(4.4, 6.4, 3.0, 4.1, 0.8, 5.7, 2.0, 3.9, 1.9, 5.7,
-             2.8, 4.1, 3.8, 1.6, 3.4, 8.4, 5.2, 6.9, 2.7, 1.9)
-rate     <- gl(2,10, labels = c("Low", "High"))
-
-Y <- data.frame(cbind(tear,gloss,opacity,rate))
-
-return_results(Y, 'rate')
